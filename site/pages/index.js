cb97ee54e232f52c0abf6fdc0ee9d74a7079abbc
Manasanan.aom <happy_nass@hotmail.com>
2018-02-13 17:53:56 +0700

    [ADD] add react

---
+++ site/pages/index.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+const Index = () => (
+    <div>
+        <h1>Hi</h1>
+        <p>18, BKK</p>
+        <ListNumber />
+    </div>
+)
+
+const ListNumber = () => (
+    <ul>
+        <li>123</li>
+        <li>123</li>
+        <li>123</li>
+        <li>123</li>
+        <li>123</li>
+        <li>123</li>
+        <li>123</li>    
+    </ul>
+)
+
+const Footer = () => (
+    <div>Footer</div>
+)
+
+export default Index
