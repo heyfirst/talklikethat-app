fa0cbaa06e962a6c2384062fa9c77ef4b249ad97
dew95531 <dew_95531@windowslive.com>
2018-02-16 12:13:49 +0700

    [Add] add userController

---
+++ api/src/main/java/com/tlt/talklikethatapi/main/UserController.java
@@ -0,0 +1,35 @@
+package com.tlt.talklikethatapi.main;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
+import com.tlt.talklikethatapi.model.User;
+import com.tlt.talklikethatapi.model.UserRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@CrossOrigin
+@Controller
+@RequestMapping(path = "/user")
+public class UserController {
+    
+    @Autowired
+    private UserRepository userRepository;
+    
+    @RequestMapping("/")
+    public @ResponseBody
+    Iterable<User> getUser() {
+        return userRepository.findAll();
+    }
+    
+//    @RequestMapping("/id")
+//    public @ResponseBody
+//    Iterable<User> getUserById(@RequestParam int id) {
+//        return userRepository.findOne();
+//    }
+    
+}
