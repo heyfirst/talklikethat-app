bfcbdf15809820927efa624e2a3fe477626ec456
dew95531 <dew_95531@windowslive.com>
2018-02-19 17:55:51 +0700

    [ADD] add ConversationRoom Model

---
+++ api/src/main/java/com/tlt/talklikethatapi/model/ConversationRoom.java
@@ -0,0 +1,51 @@
+package com.tlt.talklikethatapi.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class ConversationRoom {
+    
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private int id;
+    private int requester;
+    private int requestee;
+    private int statusID;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getRequester() {
+        return requester;
+    }
+
+    public void setRequester(int requester) {
+        this.requester = requester;
+    }
+
+    public int getRequestee() {
+        return requestee;
+    }
+
+    public void setRequestee(int requestee) {
+        this.requestee = requestee;
+    }
+
+    public int getStatusID() {
+        return statusID;
+    }
+
+    public void setStatusID(int statusID) {
+        this.statusID = statusID;
+    }
+    
+    
+}
---
+++ api/src/main/java/com/tlt/talklikethatapi/model/ConversationRoomRepository.java
@@ -0,0 +1,7 @@
+package com.tlt.talklikethatapi.model;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface ConversationRoomRepository extends CrudRepository<ConversationRoom, Integer>{
+    
+}
